4d32bda41187e5340b8122500e6ddb46d0cd9f23
hanzaa <farhan.2523@gmail.com>
2022-12-07 07:31:52 +0700

    add handlesubmit to read data from form fields

--- src/components/PopRegister.jsx
+++ src/components/PopRegister.jsx
@@ -1,4 +1,33 @@
+const base_url = process.env.REACT_APP_URL_BACKEND
+
 const PopRegister = (props) => {
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+          email: data.get('registerEmail'),
+          username: data.get('registerUsername'),
+          pass: data.get('registerPassword'),
+          confirmPass: data.get('registerConfirmPassword'),
+          
+        })
+        // try {
+        //   // 1. Lakukan Axios POST ke API Register pada backend di bawah ini
+        //   // body yang digunakan adalah username, email, dan password
+        //   const response = await axios.post(`${base_url}/register`, {
+        //     username: data.get('username'),
+        //     email: data.get('email'),
+        //     password: data.get('password')
+        //   })
+          
+        //   // jika berhasil, redirect ke halaman login
+        // } catch (error) {
+        //   // jika gagal, tampilkan alert 'Register Gagal'
+        //   alert('Register Gagal');  
+        // }
+      };
+
     return ( <>
         <div className="pop d-flex align-items-center justify-content-center">
         <div className="container login p-4 pt-3 m-5 ">
@@ -8,24 +37,25 @@ const PopRegister = (props) => {
             <div className="row d-flex justify-content-center">
                 <h3 className='fw-bold d-flex justify-content-center pt-1' style={{fontSize:"24px"}}>Gabung ke Luang</h3>
             </div>
-            <form >
-                <div class="mb-3 pt-1">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control w-100" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="masukan email"/>
+            <form onSubmit={event => handleSubmit(event)} >
+                <div className="mb-3 pt-1">
+                    <label htmlFor="registerEmail" className="form-label">Email address</label>
+                    <input type="email" className="form-control w-100" id="registerEmail" name="registerEmail" label="registerEmail" aria-describedby="emailHelp" placeholder="masukan email"/>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Username</label>
-                    <input type="text" class="form-control w-100" id="exampleInputPassword1" placeholder="masukan username"/>
+                <div className="mb-3">
+                    <label htmlFor="registerUsername" className="form-label">Username</label>
+                    <input type="text" className="form-control w-100" id="registerUsername" name="registerUsername" placeholder="masukan username"/>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control w-100" id="exampleInputPassword1" placeholder="password"/>
+                <div className="mb-3">
+                    <label htmlFor="registerPassword" className="form-label">Password</label>
+                    <input type="password" className="form-control w-100" id="registerPassword" name="registerPassword" placeholder="password"/>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-                    <input type="password" class="form-control w-100" id="exampleInputPassword1" placeholder="confirm password"/>
+                <div className="mb-3">
+                    <label htmlFor="registerConfirmPassword" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control w-100" id="registerConfirmPassword" name="registerConfirmPassword" placeholder="confirm password"/>
                 </div>
-                <button type="submit" class="btn btn-primary w-100 fw-bold" style={{background:"#FF0000",fontSize:"23px"}}>lanjut</button>
+                <button type="submit" className="btn btn-primary w-100 fw-bold" 
+                style={{background:"#FF0000",fontSize:"23px"}}>lanjut</button>
                 <p className='fw-light d-flex justify-content-center pt-1'>Sudah punya akun? .<span className='link-danger' onClick={()=> {props.popLogin(true) ;props.popRegister(false)}}>masuk</span> .</p>
             </form>
         </div>
