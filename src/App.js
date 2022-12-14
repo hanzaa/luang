9775b48a57e14de3ee58adf927ba24499b9a4a59
hanzaa <farhan.2523@gmail.com>
2022-12-14 23:05:30 +0700

    add account and pesanan route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Product from "./pages/Product";
 import BecomeSeller from "./pages/BecomeSeller";
 import Fake from "./pages/Fake";
+import Pesanan from "./pages/Pesanan";
+import Account from "./pages/Account";
 
 
 function App() {
@@ -14,7 +16,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Welcome/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
+        <Route path='/account' element={<Account/>}></Route>
         <Route path='/product/:id' element={<Product/>}></Route>
+        <Route path='/pesanan' element={<Pesanan/>}></Route>
         <Route path='/becomeseller' element={<BecomeSeller/>}></Route>
         <Route path='/fake' element={<Fake/>}></Route>
       </Routes>
