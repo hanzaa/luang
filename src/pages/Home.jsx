d8546645ee68dafa0286342660f531e6eb8c19d1
hanzaa <farhan.2523@gmail.com>
2022-12-15 15:53:54 +0700

    add category carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,11 +10,16 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 
+//mengambil link API backend dari environement variable
+const base_url = process.env.REACT_APP_URL_BACKEND;
+
 
 const Home = () => {
 
   const [loading, setLoading] = useState(false)
   const [data, setData] = useState([])
+  const [categories, setCategories] =useState([])
+
   const navigate = useNavigate()
 
   function SampleNextArrow(props) {
@@ -33,7 +38,7 @@ const Home = () => {
       return (
         <div
           className={className}
-          style={{ ...style, display: "block", background: "transparent"}}
+          style={{ ...style, display: "block", color: "green"}}
           onClick={onClick}
         />
       );
@@ -74,7 +79,7 @@ const Home = () => {
           }
         }
       ]
-  };
+  }
 
   useEffect(() => {
       setLoading(true)
@@ -89,25 +94,89 @@ const Home = () => {
       })
   }, [])
 
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`${base_url}/getcategory`)  
+    .then((res) => {
+        console.log(res)
+        setCategories(res.data)
+        setLoading(false)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}, [])
+
   return ( <>
   <div className="home"></div>
   <NavBar/>
   <div className="container pb-5">
-      <div className="row py-5">
-          <div className="col-2 me-4 p-4 " style={{background:"#D9D9D9"}}>
-              <h1 className='fw-bold' style={{fontSize:"24px"}}>Hi Fais</h1>
-              <h5 className='fw-medium' style={{fontSize:"11px"}}>Cari penjual yang cocok untukmu</h5>
-              <button type="button" className="btn btn-secondary" style={{fontSize:"11px"}}>Buat Penawaran</button>
+      <div className="row py-5 ">
+          <div className="col-2 me-4 p-4 d-flex align-items-center" style={{background:"#D9D9D9"}}>
+            <div className="row">
+              <div className="row">
+                <h1 className='fw-bold' style={{fontSize:"34px"}}>Hi {localStorage.getItem('username')}</h1>
+              </div>
+              <div className="row">
+                <h5 className='fw-medium' style={{fontSize:"16px"}}>Cari penjual yang cocok untukmu</h5>
+              </div>
+              <div className="row">
+                <button type="button" className="btn btn-secondary mt-4" 
+                style={{fontSize:"16px"}} onClick={()=>{navigate('/category')}}>Explore Products</button>
+              </div>
+            </div>
           </div>
-          <div className="col-9 bg-danger ">
-              <h3 className='p-3 ps-5 text-white fw-normal' style={{fontSize:"20px"}}>Judul Proyek</h3>
-              <h5 className='p-1 ps-5 text-white fw-normal' style={{fontSize:"14px"}}>Carousel tentang project apa aja yang direkonendasi</h5>
+          <div className="col-9 ms-5" style={{border:"solid black 1px", borderRadius:"8px"}}>
+            <div className="row" >
+              {loading && "Loading..."}
+              <Slider {...settings} 
+                dots={false} 
+                slidesToShow={1} 
+                slidesToScroll={1} 
+                arrows={false} 
+                autoplay={true} 
+                autoplaySpeed={3000}
+                infinite={true}>
+                {!!categories && categories.length>0
+                ? categories.map((category)=>{
+                  return(
+                    <div key={category.categoryid}>
+                      <h1 className='pt-4 ps-4 fw-bold '>{category.category}</h1>
+                      <div className='p-4'>
+                        <Slider {...settings } slidesToScroll={3} slidesToShow={3} 
+                        className="slick-prev:before slick-next:before" style={{color:"white"}}>
+                          {!!data && data.length > 0
+                          ? data.map((product) => {
+                            return(
+                          <div key={product.id}>
+                              <div className="card"  onClick={()=>{navigate(`/product/${product.id}`)}}>
+                                  <img src={product.images[0]} className="d-block w-100" alt="product"/>
+                                  <div className="card-body">
+                                      <h5 className="card-title fw-semibold">{product.title}</h5>
+                                      <p className="card-text">{product.description}</p>
+                                  </div>
+                              </div>
+                          </div>
+                            )})
+                          :null
+                          }
+                                              
+                        </Slider>
+                      </div>
+                    </div>
+                  
+                  )})
+                : null
+                }
+              </Slider>
+            </div>
           </div>
       </div>
       
       <div className="row" style={{border:"solid black 1px", borderRadius:"8px"}}>
-          <h3 className='p-4 fw-normal' style={{fontSize:"20px"}}>Rekomendasi untukmu</h3>
+          <h3 className='p-4 fw-bold' style={{fontSize:"30px"}}>Rekomendasi untukmu</h3>
           {loading && "Loading..."}
+          <div className='p-5'>
           <Slider {...settings}>
               {!!data && data.length > 0 
               ? data.map((product) => {
@@ -126,7 +195,7 @@ const Home = () => {
               : (<p>API did not provided any product, try again.</p>)    
               } 
           </Slider>
-          
+          </div>
       </div>
   </div>
 
